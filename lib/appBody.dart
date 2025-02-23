c606e7303da6ab6d40a0061004e7584337de85d6
ridita <ashraf15-5881@diu.edu.bd>
2025-02-23 12:59:08 +0600

    navigation

--- lib/appBody.dart
+++ lib/appBody.dart
@@ -10,12 +10,16 @@ class Appbody extends StatefulWidget {
 class _AppbodyState extends State<Appbody> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      bottomNavigationBar: BottomNavigationBar(items: [ 
+    return  Scaffold(
+      body: Center(child: pages[currentIndex]),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTapMethod,
+        currentIndex: currentIndex,
+        items: [ 
         BottomNavigationBarItem(
           icon:Icon(Iconsax.home_1_outline), label: "Home",
           ),
-        BottomNavigationBar(items: [ 
+        
         BottomNavigationBarItem(
           icon:Icon(Iconsax.home_1_outline), label: "profile",
           ),
